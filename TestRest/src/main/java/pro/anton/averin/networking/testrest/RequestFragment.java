f21c70c8600c28e8cb6f104fc2429a364cf0720c
Anton Averin <devnull@localhost>
2013-11-09 14:05:34 +0700

    View pager implemented

--- TestRest/src/main/java/pro/anton/averin/networking/testrest/RequestFragment.java
+++ TestRest/src/main/java/pro/anton/averin/networking/testrest/RequestFragment.java
@@ -1,9 +1,28 @@
 package pro.anton.averin.networking.testrest;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by AAverin on 09.11.13.
  */
-public class RequestFragment extends Fragment {
+public class RequestFragment extends ViewPagerFragment {
+
+    private View mGroupRoot;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        mGroupRoot = inflater.inflate(R.layout.fragment_request, null);
+
+        return mGroupRoot;
+    }
+
+    @Override
+    public String getPageTitle() {
+        return getString(R.string.requestViewTitle);
+    }
 }
